1ccea00bcd3a47451c99d393b4609080a6674739
ClayLipscomb <none>
2022-02-21 23:30:44 -0600

    update

--- Library/ArrayShuffle.fsx
+++ Library/ArrayShuffle.fsx
@@ -2,23 +2,23 @@ let shuffleArrayOfIntegers (sourceArray: int[])  =
     let random = System.Random()    
     /// Swap element with random element in array
     let swapElementWithRandom index arr =
-        let indexRandom = (random.Next(0, (Array.length arr) - 1))
+        let indexRandom = random.Next(maxValue = Array.length arr)
         if index = indexRandom then
             ()
         else            
             let index1Value = arr[index]
             arr[index] <- arr[indexRandom]
             arr[indexRandom] <- index1Value
+
+    let shuffledArray = sourceArray
+                        |> Array.copy
+    shuffledArray
+    |> Array.iteri (fun idx _ -> shuffledArray |> swapElementWithRandom idx)
         
-    sourceArray
-    |> Array.copy
-    |> fun shuffledArray ->
-        shuffledArray
-        |> Array.iteri (fun idx arr -> shuffledArray |> swapElementWithRandom idx)
-        shuffledArray
+    shuffledArray
         
 #time "on"
-let shuffled = [|1..100000|]
+let shuffled = [|1..100_000|]
                |> shuffleArrayOfIntegers
 shuffled
 #time "off"          
