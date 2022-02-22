6b5b6e40cfee475d7deefd58bed11a00bd0ce90b
ClayLipscomb <none>
2022-02-21 21:55:48 -0600

    Revert "update"
    This reverts commit bffd69919bebfcb77accb0b8eceb1846b7aaa3d0.

--- Library/ArrayShuffle.fsx
+++ Library/ArrayShuffle.fsx
@@ -1,25 +1,38 @@
-let shuffleArrayOfIntegers (sourceArray: int[])  =
-    let random = System.Random()    
-    /// Swap element with random element in array
-    let swapElementWithRandom index arr =
-        let indexRandom = (random.Next(0, (Array.length arr) - 1))
-        if index = indexRandom then
-            ()
-        else            
-            let index1Value = arr[index]
-            arr[index] <- arr[indexRandom]
-            arr[indexRandom] <- index1Value
-        
-    sourceArray
-    |> Array.copy
-    |> fun shuffledArray ->
-        shuffledArray
-        |> Array.iteri (fun idx arr -> shuffledArray |> swapElementWithRandom idx)
-        shuffledArray
-        
+
+module Shuffle 
 #time "on"
-let shuffled = [|1..100000|]
-               |> shuffleArrayOfIntegers
-shuffled
+
+//module ArrayShuffle =       
+    ///let rnd = System.Random(3)
+    //let rndNmbr = rnd.Next(0,9 )    
+//    let coll = [1..10]
+//    let shuffled = coll
+//                   |> Array.permute (fun i -> rnd.Next(0, 9))
+//    let shuffled3 = coll
+//                    |> Array.permute (fun i -> (i + 3) % 10)
+//    let shuffledSort = coll
+//                       |> Seq.sortBy (fun _ -> rnd.Next())
+//                       |> Array.ofSeq
+
+let shuffleBySort randomizerSeed (sourceArray: int[]) =
+    let rnd = System.Random(randomizerSeed)
+    sourceArray
+    |> Array.sortBy (fun _ -> rnd.Next())
+    
+let shuffleArrayOfIntegers (sourceArray: int[]) =
+    /// Swap two elements in array
+    let swapElements index1 index2 (arr: int []) =
+        let index1Value = arr[index1]
+        arr[index1] <- arr[index2]
+        arr[index2] <- index1Value
+        //Array.set arr index2 index1Value
+        arr        
+    sourceArray
+        
+let arr = [|1..1000|]
+let shuffled =
+    arr
+    |> shuffleBySort 3
+    
 #time "off"
             
